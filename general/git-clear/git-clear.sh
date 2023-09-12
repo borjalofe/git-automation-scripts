6a55073838e282c14ed25aad5700d39052a695ba
Borja López <blopezf@bedisruptive.com>
2023-09-12 11:53:43 +0200

    feat: :sparkles: Add shortcut for when there are no branches to delete

--- general/git-clear/git-clear.sh
+++ general/git-clear/git-clear.sh
@@ -2,6 +2,12 @@
 
 BRANCHES_TO_DELETE=$(git branch | grep -v develop | grep -v master | grep -v main | grep -v release)
 
+if [ -z "$BRANCHES_TO_DELETE" ]
+then
+    echo "No branches to delete"
+    exit 0
+fi
+
 echo "You have currently $(echo $BRANCHES_TO_DELETE | wc -l) branches to delete: $BRANCHES_TO_DELETE"
 read -p "Are you sure you want to delete them? (y/n) " -n 1 -r
 
